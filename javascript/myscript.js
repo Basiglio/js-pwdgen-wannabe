cc6530b63cc5f456812baa6366a480ffa59884b1
Emilo <emilio.furnari87@gmail.com>
2020-11-10 15:27:58 +0100

    completato tolti i debug e commentato

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,19 +1,16 @@
 
-
-
-
+// VARIABILI SINGOLE
+// PROMPT
 var name = prompt("Qual'è il tuo nome??");
-console.log(name)
 
 var surname = prompt("Qual'è il tuo cognome??");
-console.log(surname)
 
 var favouriteColor = prompt("Qual'è il tuo colore preferito??");
-console.log(favouriteColor)
 
 var diciannove = 19;
-
+// SOMMA DELLE VARIBILI
 var password = name + surname + favouriteColor + diciannove;
-console.log(password)
 
+
+// RISULTATO VARIABILI SCRITTO NEL HTML
 document.getElementById('password').innerHTML=password;
