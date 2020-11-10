669da3f0f09c6ad71d051aead3e36cefecd02f4e
Emilo <emilio.furnari87@gmail.com>
2020-11-10 15:05:15 +0100

    quarto push/ testo il prompt

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,1 +1,10 @@
 document.getElementById('password').innerHTML='Hello World!';
+
+
+var name = prompt("Qual'è il tuo nome??");
+
+var surname = prompt("Qual'è il tuo cognome??");
+
+var favouriteColor = prompt("Qual'è il tuo colore preferito??");
+
+var password =
