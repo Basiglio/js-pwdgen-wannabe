49d5ce51d8eb4c900b7e52d9b0b525e558b6850b
Emilo <emilio.furnari87@gmail.com>
2020-11-10 15:23:48 +0100

    completato

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,10 +1,19 @@
-document.getElementById('password').innerHTML='Hello World!';
+
+
 
 
 var name = prompt("Qual'è il tuo nome??");
+console.log(name)
 
 var surname = prompt("Qual'è il tuo cognome??");
+console.log(surname)
 
 var favouriteColor = prompt("Qual'è il tuo colore preferito??");
+console.log(favouriteColor)
 
-var password =
+var diciannove = 19;
+
+var password = name + surname + favouriteColor + diciannove;
+console.log(password)
+
+document.getElementById('password').innerHTML=password;
